8cf5570d0844ec9efe0d99cc6856de74166d37a5
Vandad Nahavandipoor <vandad.np@gmail.com>
2021-08-29 07:08:53 +0200

    Delete using-nested-paths-in-rust.rs

--- images/using-nested-paths-in-rust.rs
+++
@@ -1,9 +0,0 @@
-// Want to support my work 🤝? https://buymeacoffee.com/vandad
-
-// 👇🏻 instead of this
-use std::io;
-use std::io::Result as IOResult;
-use std::io::Seek;
-
-// 👇🏻 do this
-use std::io::{self, Result as IOResult, Seek};
